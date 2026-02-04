cc3ab72fdd4b08c9808bcc5db3412faca15b5233
edinbolvito721-gif <edinbolvito721@gmail.com>
2026-02-03 19:18:48 -0700

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,6 +1,1 @@
-module.exports = {
-    run: async (sock, m, text) => {
-        const id = m.key.remoteJid;
-        await sock.sendMessage(id, { text: '¡Sistema de comandos (ContenedorEventos) activo! 🚀' });
-    }
-};
+
